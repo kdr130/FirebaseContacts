9b979fdfaca018b2628d90cf5aac6f9ad40b9d33
Kevin <kdr0130@gmail.com>
2018-05-01 02:30:30 +0800

    Add creating user but failed. auth.CreateUserWith... not working.

--- app/src/main/java/com/example/kevin/contacts/LoginActivity.java
+++ app/src/main/java/com/example/kevin/contacts/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.example.kevin.contacts;
 
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -52,12 +57,58 @@ public class LoginActivity extends AppCompatActivity {
 
     public void login(View v) {
         Log.d(TAG, "login: btn login clicked");
-        String email = ((EditText)findViewById(R.id.email)).getText().toString();
-        String password = ((EditText)findViewById(R.id.password)).getText().toString();
+        final String email = ((EditText)findViewById(R.id.email)).getText().toString();
+        final String password = ((EditText)findViewById(R.id.password)).getText().toString();
 
         Log.d(TAG, "login: email: " + email + ", password: " + password);
 
         // using firebaseAuto to check email and password
-        auth.signInWithEmailAndPassword(email, password);
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            // 登入工作完成後會 callback 的 method
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                // check is registered user
+                if (!task.isSuccessful()) {
+                    Log.d(TAG, "onComplete: login failed");
+                    register(email, password);
+                }
+            }
+        });
     }
+
+    private void register(final String email, final String password) {
+        new AlertDialog.Builder(this)
+                .setTitle("登入問題")
+                .setMessage("沒有該帳號，是否註冊為使用者?")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.d(TAG, "AlertDialog PositiveButton onClick: ");
+                        createUser(email, password);
+                    }
+                })
+                .setNeutralButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.d(TAG, "AlertDialog NeutralButton onClick: ");
+                    }
+                })
+                .show();
+    }
+
+    private void createUser(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        String message = task.isSuccessful() ? "註冊成功" : "註冊失敗" ;
+                        new AlertDialog.Builder(LoginActivity.this)
+                                .setMessage(message)
+                                .setPositiveButton("ok", null)
+                                .show();
+                    }
+                });
+    }
+
+
 }
